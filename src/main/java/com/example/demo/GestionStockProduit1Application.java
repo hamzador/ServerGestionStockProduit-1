ffa9df54cd4ef8c68eaa11fe713baa37cd77b9d7
hamzador <hamozadouyry@gmail.com>
2019-11-12 19:15:48 +0100

    1er commit

---
+++ src/main/java/com/example/demo/GestionStockProduit1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionStockProduit1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionStockProduit1Application.class, args);
+	}
+
+}
